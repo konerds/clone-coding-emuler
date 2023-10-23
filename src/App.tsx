44e06961ba022cedf7b0d4acd6c8aa020184646e
Konerd <adr10won@gmail.com>
2023-10-23 11:26:47 +0900

    fix wrong link favicon

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 const App = () => {
-    return <div>Hello World!</div>;
+    return <div className={`${process.env.NODE_ENV !== 'production' ? 'debug-screens' : ''}`}>Hello World!</div>;
 };
 
 export default App;
