f720f954e5b69af6de36df4cb802c5cfed909ea0
Konerd <adr10won@gmail.com>
2023-10-23 11:00:41 +0900

    migrate tailwindcss setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
 /** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
+
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        screens: {
+            ...defaultTheme.screens,
+            xxs: '0px',
+            xs: '420px',
+            lg: '1024px',
+            xl: '1200px',
+        },
+        extend: {
+            colors: {
+                green: colors.emerald,
+                yellow: colors.amber,
+                purple: colors.violet,
+                gray: {
+                    ...colors.neutral,
+                    100: '#C4C4C4',
+                    200: '#666666',
+                    300: '#555555',
+                    400: '#111111',
+                },
+                'shuttle-gray': '#5E6774',
+                harp: '#E9EFF1',
+            },
+        },
+    },
+    future: {
+        hoverOnlyWhenSupported: true,
     },
     plugins: [],
 };
