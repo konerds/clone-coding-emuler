fbfda9c4374a8855ef244f2e4108b5967fc11e33
Konerd <adr10won@gmail.com>
2023-10-31 18:32:45 +0900

    fix public url problem

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,10 +1,18 @@
-import axios from 'axios';
-import { IObjMenuHeader } from '../interface';
+import axios, { AxiosInstance } from 'axios';
+import { IObjBenefit, IObjMenuHeader } from '../interface';
+
+const instanceAxios: AxiosInstance = axios.create({
+  baseURL:
+    process.env.NODE_ENV === 'development' ||
+    process.env.REACT_APP_IS_USE_PROXY === 'true'
+      ? '/api'
+      : `${process.env.REACT_APP_URL_API_SERVER}/api`,
+});
 
 export const getListObjMenuHeader = async () => {
   try {
-    const response = await axios.get<IObjMenuHeader[]>(
-      '/api/resource/menu-header',
+    const response = await instanceAxios.get<IObjMenuHeader[]>(
+      '/resource/menu-header',
     );
     return response.data;
   } catch (e) {}
@@ -12,7 +20,8 @@ export const getListObjMenuHeader = async () => {
 
 export const getListObjBenefit = async () => {
   try {
-    const response = await axios.get<IObjMenuHeader[]>('/api/resource/benefit');
+    const response =
+      await instanceAxios.get<IObjBenefit[]>('/resource/benefit');
     return response.data;
   } catch (e) {}
 };
--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,14 +1,23 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
-  app.use(
-    '/api',
-    createProxyMiddleware({
-      target:
-        process.env.NODE_ENV === 'development'
-          ? 'http://localhost:3010'
-          : process.env.URL_API_SERVER,
-      changeOrigin: true,
-    }),
-  );
+  if (process.env.NODE_ENV === 'development') {
+    app.use(
+      '/api',
+      createProxyMiddleware({
+        target: 'http://localhost:3010',
+        changeOrigin: true,
+      }),
+    );
+  } else {
+    if (process.env.REACT_APP_IS_USE_PROXY === 'true') {
+      app.use(
+        '/api',
+        createProxyMiddleware({
+          target: process.env.REACT_APP_URL_API_SERVER,
+          changeOrigin: true,
+        }),
+      );
+    }
+  }
 };
