33cb882476203751b519168b24ad5f6e659d24be
Konerd <adr10won@gmail.com>
2024-10-08 16:39:32 +0900

    chore: Change webpack proxy config to only use local web server in local dev environment

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,18 +1,19 @@
 const apiMocker = require('connect-api-mocker');
-const { createProxyMiddleware } = require('http-proxy-middleware');
+// const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = (app) => {
   if (process.env.NODE_ENV === 'development') {
     app.use(apiMocker('/api', 'mocks/api'));
   } else {
-    if (process.env.REACT_APP_IS_USE_PROXY === 'true') {
-      app.use(
-        '/api',
-        createProxyMiddleware({
-          target: process.env.REACT_APP_URL_API_SERVER,
-          changeOrigin: true,
-        }),
-      );
-    }
+    /**
+     * For test with real server in local
+     */
+    // app.use(
+    //     '/api',
+    //     createProxyMiddleware({
+    //       target: process.env.REACT_APP_URL_API_SERVER,
+    //       changeOrigin: true,
+    //     }),
+    //   );
   }
 };
